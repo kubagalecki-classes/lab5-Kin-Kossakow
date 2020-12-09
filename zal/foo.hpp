146e5f51f3068f25ec41d785056499e43983d51c
Kin-Kossakow <73644765+Kin-Kossakow@users.noreply.github.com>
2020-12-09 11:20:50 +0100

    Update foo.hpp

--- zal/foo.hpp
+++ zal/foo.hpp
@@ -7,6 +7,17 @@
 
 std::vector< char > foo(std::list< Human >& people)
 {
-    // Twoja implementacja tutaj
-    return {};
+    std::vector< char > tmp;
+    std::list< Human >::reverse_iterator rit = people.rbegin();
+    while (rit != people.rend()) {
+        rit->birthday();
+        if (rit->isMonster() == true) {
+            tmp.push_back('n');
+        }
+        else {
+            tmp.push_back('y');
+        }
+        *rit++;
+    }
+    return {tmp};
 }
